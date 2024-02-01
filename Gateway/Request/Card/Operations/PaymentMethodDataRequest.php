2645b2eff0db156686518266efeef210496fe31f
Elisei <brunoelisei@o2ti.com>
2024-02-01 11:13:16 -0300

    Tratamento de erro

--- Gateway/Request/Card/Operations/PaymentMethodDataRequest.php
+++ Gateway/Request/Card/Operations/PaymentMethodDataRequest.php
@@ -9,8 +9,6 @@
 namespace Getnet\PaymentMagento\Gateway\Request\Card\Operations;
 
 use InvalidArgumentException;
-use Getnet\PaymentMagento\Gateway\Config\ConfigCc;
-use Getnet\PaymentMagento\Gateway\SubjectReader;
 use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
 use Magento\Payment\Gateway\Request\BuilderInterface;
 
@@ -25,28 +23,6 @@ class PaymentMethodDataRequest implements BuilderInterface
     public const PAYMENT_METHOD = 'payment_method';
 
     /**
-     * @var SubjectReader
-     */
-    protected $subjectReader;
-
-    /**
-     * @var ConfigCc
-     */
-    protected $configCc;
-
-    /**
-     * @param SubjectReader $subjectReader
-     * @param ConfigCc      $configCc
-     */
-    public function __construct(
-        SubjectReader $subjectReader,
-        ConfigCc $configCc
-    ) {
-        $this->subjectReader = $subjectReader;
-        $this->configCc = $configCc;
-    }
-
-    /**
      * Build.
      *
      * @param array $buildSubject
@@ -59,20 +35,8 @@ class PaymentMethodDataRequest implements BuilderInterface
             throw new InvalidArgumentException('Payment data object should be provided');
         }
 
-        $paymentDO = $this->subjectReader->readPayment($buildSubject);
-
-        $order = $paymentDO->getOrder();
-
-        $storeId = $order->getStoreId();
-
-        $method = 'CREDIT';
-
-        if ($this->configCc->hasDelayed($storeId)) {
-            $method = 'CREDIT_AUTHORIZATION';
-        }
-
         return [
-            self::PAYMENT_METHOD => $method,
+            self::PAYMENT_METHOD => 'CREDIT_AUTHORIZATION',
         ];
     }
 }
